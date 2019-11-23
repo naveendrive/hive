8415c98d90896913d9b38f1884245ea7c1d9dd4b
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-11-23 13:16:30 +0530

    Add files via upload

---
+++ scenarios/1_time_stamp_example.sql
@@ -0,0 +1,17 @@
+-- Create table with timestamp
+CREATE TABLE time_sample(id Int,name 
+	String,
+	doj timestamp) 
+ROW FORMAT DELIMITED 
+FIELDS TERMINATED BY ',';
+
+-- Load data into the table
+LOAD DATA LOCAL INPATH '/home/training/Desktop/time_sample.txt' INTO TABLE time_sample;
+
+--We need to convert the time field to YYYY-MM-DD to be able to work with hive timestamp;
+
+--Get next 15 days records.
+SELECT * FROM time_sample WHERE DATEDIFF(current_date,doj) > -15 AND DATEDIFF(current_date,doj) < 0;
+
+SELECT * FROM time_sample WHERE doj BETWEEN '2019-02-01 00:00:00' AND '2019-03-01 00:00:00';
+
